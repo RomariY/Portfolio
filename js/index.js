1a87233e07b8f609f252b5653befb89dc73aa226
RomariY <yaremakroma@gmail.com>
2021-03-23 23:13:20 +0200

    hover trouble

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// // Header
+
+// let topPage = document.querySelector("#topPage");
+// let centerText = document.querySelector(".centerText");
+// let headerImg = document.querySelector(".headerImg");
+// let bottomFade = document.querySelector(".centerFade");
+// let bottomText = document.querySelector(".bottomText");
+
+// window.addEventListener('scroll', function () {
+//     let value = window.scrollY;
+//     topPage.style.bottom = value * 0.5 + 'px';
+//     centerText.style.top = value * 0.5 + 'px';
+//     headerImg.style.bottom = value * 0.5 + 'px';
+//     bottomFade.style.bottom = value * 0.5 + 'px';
+//     bottomText.style.bottom = value * 0.5 + 'px';
+// });
