b30b3c72b46d9c64e8f14c1c7b8c36b4d0926255
RomariY <yaremakroma@gmail.com>
2021-05-12 14:32:27 +0300

    Prevent scroll

--- js/index.js
+++ js/index.js
@@ -1,3 +1,42 @@
+//Scrolling
+function preventDefault(e) {
+    e.preventDefault();
+  }
+  
+  function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+      preventDefault(e);
+      return false;
+    }
+  }
+  
+  // modern Chrome requires { passive: false } when adding event
+  var supportsPassive = false;
+  try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+      get: function () { supportsPassive = true; } 
+    }));
+  } catch(e) {}
+  
+  var wheelOpt = supportsPassive ? { passive: false } : false;
+  var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+  
+  // call this to Disable
+  function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+  }
+  
+  // call this to Enable
+  function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+  }
+
 //Preloader
 
 document.querySelector("body").style.visibility = "hidden";
@@ -21,10 +60,12 @@ menuBar.addEventListener('click', _=>{
         menuBar.classList.add('open');
         menuOpen = true;
         document.querySelector(".menu").classList.add('active');
+        disableScroll()
     } else {
         menuBar.classList.remove('open');
         document.querySelector(".menu").classList.remove('active');
         menuOpen = false;
+        enableScroll()
     }
 });
 
